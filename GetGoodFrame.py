170bb034bef667c589b0a8ae1481814aa84f18e2
Venkat Arun <venkatarun95@gmail.com>
2015-02-20 21:43:16 +0530

    GetGoodFrames.py    - Change is now calculated using difference in sobel edge-detected images    - Change is calculated b/w current frame and last (in depth) processed frame, instead of cumulative change from the last seen frame.    - An evolving gaussian estimator is used to estimate how much to sleep.w

--- GetGoodFrame.py
+++ GetGoodFrame.py
@@ -7,6 +7,7 @@ Computation Module:
 import cv2
 import numpy as np
 from scipy import ndimage
+import time
 
 from Computation import Computation
 from GetFrame import GetFrame
@@ -18,10 +19,16 @@ class GetGoodFrame(Computation):
 	prevFrame = None
 
 	"""Threshold after shich sufficient change is assumed to have occured"""
-	changeThreshold = 500
-	"""The current amount of change that has gone unprocessed"""
-	curChangeAmount = 0
-	
+	changeThreshold = 10
+	"""The acceptable probability of exceeding the changeThreshold given as number of standard deviations"""
+	probError = 2
+	""""Estimated mean of normally distributed change per unit time"""
+	meanChangeRate = 1 #some arbitrary value. Will converge to more sensible values
+	""""Estimated variance of normally distributed change per unit time"""
+	varChangeRate = 1 #some arbitrary value that will converge to sensible value as system evolves
+	""""Time stamp of the last time the function was called"""
+	prevAwakeTime = time.time()
+
 	"""If set to True, the least 'blurry' frame will be used"""
 	analyseBlurriness = True
 	"""The least blurry frame as measured using average edge strength"""
@@ -32,29 +39,57 @@ class GetGoodFrame(Computation):
 
 	def run(self, params):
 		"""override: If there has been sufficient change in scene and the frame is not blurry return the frame. Else returns None"""
-		print "Considering frame 932864", self.curChangeAmount, self.bestBlurrinessScore
-		frame = ndimage.gaussian_filter(params[0][0], sigma=5)
+		#print "Considering frame 932864", self.curChangeAmount, self.bestBlurrinessScore
+		#frame = cv2.cvtColor(ndimage.gaussian_filter(params[0][0], sigma=5), cv2.COLOR_BGR2GRAY)
+		frame1 = cv2.GaussianBlur(params[0][0], (3,3), 0)
+		frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+	
+		scale = 1
+		delta = 0
+		ddepth = cv2.CV_16S	
+		grad_x = cv2.Sobel(frame1,ddepth,1,0,ksize = 3, scale = scale, delta = delta,borderType = cv2.BORDER_DEFAULT)
+		#grad_x = cv2.Scharr(gray,ddepth,1,0)
+		# Gradient-Y
+		grad_y = cv2.Sobel(frame1,ddepth,0,1,ksize = 3, scale = scale, delta = delta, borderType = cv2.BORDER_DEFAULT)
+		#grad_y = cv2.Scharr(gray,ddepth,0,1)
+		abs_grad_x = cv2.convertScaleAbs(grad_x) # converting back to uint8
+		abs_grad_y = cv2.convertScaleAbs(grad_y)
+		frame1 = cv2.addWeighted(abs_grad_x,0.5,abs_grad_y,0.5,0)
+
+		frame1 = cv2.threshold(frame1, 50, 255, cv2.THRESH_TOZERO)[1]
+		
 		if self.prevFrame == None:
-			self.prevFrame = frame
+			self.prevFrame = frame1
 			return None
 		
-		meanDiff = np.mean(frame.flatten() - self.prevFrame.flatten())
-		self.prevFrame = frame
-		self.curChangeAmount += meanDiff
+		meanDiff = np.mean(np.abs(frame1.flatten() - self.prevFrame.flatten()))
+		print "Analysing frame 238456", meanDiff, self.meanChangeRate
+
+		#Analyse meanDiff and pause for a while
+		curTime = time.time()
+		meanDiffPerUnitTime = meanDiff/(curTime - self.prevAwakeTime)
+		self.prevAwakeTime = curTime
+
+		self.varChangeRate = (self.varChangeRate + (meanDiffPerUnitTime - self.meanChangeRate)**2)/2
+		self.meanChangeRate = (self.meanChangeRate + meanDiffPerUnitTime)/2
+		if meanDiff < self.changeThreshold:
+			if self.meanChangeRate - self.varChangeRate*self.probError < 0.001: #anyway most operating systems (esp. linux, windows is better) will not be able to sleep for less than 1ms
+				return None
+			time.sleep(self.changeThreshold/(self.meanChangeRate - self.varChangeRate*self.probError))
+			return None
+
 
 		if self.analyseBlurriness:
-			edgeStrength = np.mean(ndimage.sobel(frame, axis=0, mode='constant'))
-			edgeStrength += np.mean(ndimage.sobel(frame, axis=1, mode='constant'))
-			
+			#edgeStrength = np.mean(ndimage.sobel(frame, axis=0, mode='constant'))
+			#edgeStrength += np.mean(ndimage.sobel(frame, axis=1, mode='constant'))
+			edgeStrength = np.mean(frame1)
 			if edgeStrength >= self.bestBlurrinessScore:
 				self.bestBlurrinessScore = edgeStrength
 				self.bestFrame = params[0]
 
-		if self.curChangeAmount < self.changeThreshold:
-			return None
-
 		self.bestBlurrinessScore = -1
 		self.curChangeAmount = 0
+		self.prevFrame = frame1
 		print "Frame accepted 42243"
 		return params[0]
 
